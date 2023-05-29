3f9d9d7604eb93dcaf39f9aef06bc7ec023c4018
Michael A. Yount <cheshirekatsmile101@gmail.com>
2023-05-29 18:11:52 -0400

    fixed HashRouter

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -4,8 +4,8 @@ function NavBar(){
     <div className="container-fluid">
     <nav className="navbar navbar-expand-xl navbar-dark ">
       
-    <div className="brand"><img src="../img/badBank.ico" alt=""/></div>
-    <a className="navbar-brand" href="../pages/Home.jsx">
+    <div className="brand"><img src="../img/favicon-32x32.png" alt=""/></div>
+    <a className="navbar-brand" href="#/Home">
       My <span className="bad">Bad</span> Bank</a>
       
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
@@ -13,27 +13,28 @@ function NavBar(){
     </button>
     <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
       <ul className="navbar-nav">
-      <li className="navbar-item">
-        <a className="nav-link active" aria-current="page" href="../pages/Login.jsx">Login</a>
+        <li className="navbar-item">
+         
+            <a className="nav-link active " aria-current="page"  href="#/Login">Login</a>
         </li>
         <li className="navbar-item">
-        <a className="nav-link active" aria-current="page" href="../pages/Deposit.jsx">Deposit</a>
+        <a className="nav-link active" aria-current="page" href="#/Deposit">Deposit</a>
         </li>
         <li className="navbar-item">
-        <a className="nav-link active" aria-current="page" href="../pages/Withdraw.jsx">Withdraw</a>
+        <a className="nav-link active" aria-current="page" href="#/Withdraw">Withdraw</a>
         </li>
         <li className="navbar-item">
-        <a className="nav-link active" aria-current="page" href="../pages/AllData.jsx">Data</a>
+        <a className="nav-link active" aria-current="page" href="#/AllData">Data</a>
         </li>
         <li className="navbar-item">
-        <a className="nav-link active" aria-current="page" href="../pages/Home.jsx">Home</a>
+        <a className="nav-link active" aria-current="page" href="#/Home">Home</a>
         </li>
        
       </ul>
     </div>
 </nav>
     </div>
-          , 
+           
         </>
     );
 }
