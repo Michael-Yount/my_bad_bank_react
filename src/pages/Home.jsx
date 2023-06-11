4b6e7d0d1cb5da5a7ad0af0fc073dba38b63a901
Michaeal Yount <cheshirekatsmile101@gmail.com>
2023-06-10 20:25:45 -0400

    added card to home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,25 +1,26 @@
 import { useContext } from 'react';
 import { UserContext } from '../App';
 import BankForm from './BankForm';
+import { Card, Container } from 'react-bootstrap';
 import pic from '../badbank.png'
 
 function Home() {
   const ctx = useContext(UserContext);
 
   return (
-    <div className="container">
+    <Container className="container">
       <p className="welcome"><span className='welcome-word'>Welcome</span> to My Bad Bank, where your money is our money. No secrets, No security, thats our motto.</p>
       <h1 className='header'>My <span style={{color: 'red'}}>Bad</span> Bank</h1>
       {ctx.loggedInUser ? (
         <p>Logged in user: <b className="capitalize">{ctx.loggedInUser}</b>.</p>
         ) : (
-        
+        <Card className='card'>
           <BankForm
           formName="Login"
           />
-
+        </Card>
           )}
-    </div>
+    </Container>
   )
 }
 
